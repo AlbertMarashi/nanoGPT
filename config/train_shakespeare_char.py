6e5066ed3493e7f8331ba018c356485a079b9612
Albert <albert@lumina.earth>
2024-04-20 01:04:10 +0930

    No change

--- config/train_shakespeare_char.py
+++ config/train_shakespeare_char.py
@@ -3,7 +3,7 @@
 
 out_dir = 'out-shakespeare-char'
 eval_interval = 250 # keep frequent because we'll overfit
-eval_iters = 200
+eval_iters = 100
 log_interval = 10 # don't print too too often
 
 # we expect to overfit on this small dataset, so only save when val improves
@@ -16,17 +16,17 @@ wandb_run_name = 'mini-gpt'
 dataset = 'shakespeare_char'
 gradient_accumulation_steps = 1
 batch_size = 12
-block_size = 128 # context of up to 256 previous characters
+block_size = 64 # context of up to 256 previous characters
 
 # baby GPT model :)
-n_layer = 4
+n_layer = 1
 n_head = 4
-n_embd = 64
+n_embd = 256
 dropout = 0.0
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
 max_iters = 2000
-lr_decay_iters = 1000 # make equal to max_iters usually
+lr_decay_iters = 2000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
--- model.py
+++ model.py
@@ -142,7 +142,7 @@ class GPT(nn.Module):
         # apply special scaled init to the residual projections, per GPT-2 paper
         for pn, p in self.named_parameters():
             if pn.endswith('c_proj.weight'):
-                torch.nn.init.normal_(p, mean=0.0, std=0.02/math.sqrt(2 * config.n_layer))
+                torch.nn.init.normal_(p, mean=0.0, std=0.02/math.sqrt(2 * config.n_layer * 4))
 
         # report number of parameters
         print("number of parameters: %.2fM" % (self.get_num_params()/1e6,))
