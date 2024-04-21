ad8c5b089d7ca3fa14dc217e58364e1f35656fa8
Albert <albert@lumina.earth>
2024-04-21 21:39:14 +0930

    update some settings

--- config/train_shakespeare_char.py
+++ config/train_shakespeare_char.py
@@ -15,8 +15,8 @@ wandb_run_name = 'mini-gpt'
 
 dataset = 'shakespeare_char'
 gradient_accumulation_steps = 1
-batch_size = 16
-block_size = 256
+batch_size = 24
+block_size = 512
 
 # baby GPT model :)
 n_layer = 0
@@ -25,8 +25,8 @@ n_embd = 8
 dropout = 0.0
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 2000
-lr_decay_iters = 2000 # make equal to max_iters usually
+max_iters = 3000
+lr_decay_iters = 3000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
