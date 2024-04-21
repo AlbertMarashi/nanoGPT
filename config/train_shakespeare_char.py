aa46e09e0db464c2560cb561b57113f87abe92f9
Albert <albert@lumina.earth>
2024-04-21 21:04:15 +0930

    Larger diff

--- config/train_shakespeare_char.py
+++ config/train_shakespeare_char.py
@@ -3,7 +3,7 @@
 
 out_dir = 'out-shakespeare-char'
 eval_interval = 250 # keep frequent because we'll overfit
-eval_iters = 100
+eval_iters = 200
 log_interval = 10 # don't print too too often
 
 # we expect to overfit on this small dataset, so only save when val improves
@@ -15,23 +15,23 @@ wandb_run_name = 'mini-gpt'
 
 dataset = 'shakespeare_char'
 gradient_accumulation_steps = 1
-batch_size = 12
-block_size = 64 # context of up to 256 previous characters
+batch_size = 24
+block_size = 512 # context of up to 256 previous characters
 
 # baby GPT model :)
 n_layer = 1
-n_head = 4
-n_embd = 256
+n_head = 1
+n_embd = 8
 dropout = 0.0
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 2000
-lr_decay_iters = 2000 # make equal to max_iters usually
+max_iters = 4000
+lr_decay_iters = 3000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
 warmup_iters = 100 # not super necessary potentially
 
 # on macbook also add
-device = 'cpu'  # run on cpu only
+device = 'mps'  # run on cpu only
 compile = False # do not torch compile the model
